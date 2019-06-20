2b1587a3ebf76d3ecbdfc6349d6a05a21c75501a
Mr7aMoDy <51889681+Mr7aMoDy@users.noreply.github.com>
2019-06-20 15:41:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['£help','GalaxyShop'];
+    var setGame = ['£help','WolfShop'];
     var i = -1;
     var j = 0;
     setInterval(function (){
